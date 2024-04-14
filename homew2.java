78d3e8c9da441c44d22c67d551bb335882584a22
Ammars24 <160650867+Ammars24@users.noreply.github.com>
2024-04-14 12:13:38 +0200

    Add files via upload
    home work 2

---
+++ homew2.java
@@ -0,0 +1,21 @@
+import java.sql.SQLOutput;
+
+public class homew2 {
+    public static void main(String[] args) {
+        /*double a= 5,b=8,c=2;
+
+        System.out.println(a+b+c );
+        System.out.println(a-b );
+        System.out.println(a+b*c );
+        System.out.println(a+b/c );
+        System.out.println(a%c );
+        */
+        int d = 345;
+        String  dString =String.valueOf(d);
+        System.out.println(dString.charAt(0) + " " +dString.charAt(2));
+
+
+
+
+    }
+}
